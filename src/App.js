30ab31fec9f7e8a3b182e40f5f9b1caaede2d95a
Tucker Bradford <github@tuckerbradford.com>
2021-05-29 14:09:51 -0400

    adds disappearing flash

--- src/App.js
+++ src/App.js
@@ -1,37 +1,42 @@
 import "./App.css";
+import Bus from "./utils/bus.js";
 import React, { useEffect, useState } from "react";
 import checkAnswer from "./lib/getAnswer.js";
 import Flash from "./Flash.js";
 
-function App() {
-  const [question, setQuestion] = useState("");
+const App = () => {
+  window.flash = (message, type = "flash hide-opacity") =>
+    Bus.emit("flash", { message, type });
+
+  const [question, setQuestion] = useState("…loading");
   const [questionId, setQuestionId] = useState(1);
   const [currentAnswer, setCurrentAnswer] = useState("");
-  const [error, setError] = useState("");
 
   const handleClick = () => () => {
-    setError(undefined);
-    checkAnswer(questionId, currentAnswer, (question, questionId, error) => {
+    checkAnswer(questionId, currentAnswer, (question, questionId, s = {}) => {
+      setCurrentAnswer("");
       setQuestion(question);
       setQuestionId(questionId);
-      setError(error);
+      window.flash(s.message, s.status + " flash hide-opacity");
     });
   };
 
   useEffect(() => {
-    checkAnswer(questionId, currentAnswer, (question, questionId, error) => {
+    checkAnswer(questionId, currentAnswer, (question, questionId) => {
       setQuestion(question);
       setQuestionId(questionId);
     });
-  }, []);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
   return (
     <div className="App">
       <header className="App-header">Trash Panda Online Scavenger Hunt</header>
-      <Flash message={error} />
+      <Flash />
       <h1> {question} </h1>
       <textarea
         id={questionId}
         onChange={(e) => setCurrentAnswer(e.target.value)}
+        value={currentAnswer}
       ></textarea>
       <br />
       <br />
@@ -40,6 +45,6 @@ function App() {
       </button>
     </div>
   );
-}
+};
 
 export default App;
--- src/Flash.js
+++ src/Flash.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./Flash.css";
+import Bus from "./utils/bus.js";
 
-const Flash = ({ message }) => {
-  if (message !== null && message !== "" && message !== undefined) {
-    return (
-      <div id="error-flash" className="error hide-opacity">
-        {message}
-      </div>
-    );
-  } else {
-    return <div></div>;
-  }
+const Flash = () => {
+  let [visibility, setVisibility] = useState(false);
+  let [message, setMessage] = useState("");
+  let [type, setType] = useState("");
+
+  useEffect(() => {
+    Bus.addListener("flash", ({ message, type }) => {
+      setVisibility(true);
+      setMessage(message);
+      setType(type);
+      setTimeout(() => {
+        setVisibility(false);
+      }, 4000);
+    });
+  }, []);
+
+  const body = () => {
+    if (visibility) {
+      return (
+        <div id="flash" className={type}>
+          {message}
+        </div>
+      );
+    } else {
+      return <div id="flash"> &nbsp; </div>;
+    }
+  };
+  return body();
 };
 
 export default Flash;
